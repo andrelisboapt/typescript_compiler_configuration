7f2e56909f8a31598004923e0efc7b0afb9bd76d
André Lisboa <andrelisboapt@hotmail.com>
2023-05-30 19:22:30 +0100

    compilation options (target, TS core libs, source maps)

--- app.ts
+++ app.ts
@@ -1,5 +1,15 @@
+/* let age: number; // this will create a "let" instead of "var" variable due to the compiler target "es2016"
+age = 30;
+
 const userName = 'Maximilian';
 
 
 console.log(userName)
 
+ */
+
+const button = document.querySelector('button')!; //since Typescript is not sure if this button exists, we can add a "!" just telling Typescript to not worry that this button will eventually exist
+
+button.addEventListener('click', () => {
+    console.log('Clicked!')
+});
